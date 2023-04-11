5b55bb31e47c2d6413ce94ce4907c77f923444c5
Brody14 <stefania.spinas@gmail.com>
2023-04-11 15:30:50 +0200

    FEAT print movies

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function homepage()
     {
-        return view('welcome');
+        $movies = Movie::all();
+
+        $data = [
+            'movies' => $movies
+        ];
+
+        return view('welcome', $data);
     }
 }
